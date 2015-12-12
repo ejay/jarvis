1300e08d39fa159e2b63c10a9c7318b660b5854d
Bas de Bruijn <s.f.debruijn@gmail.com>
2015-12-12 22:40:33 +0100

    add playing song to control

--- uc-raspberry/src/main/java/control/Main.java
+++ uc-raspberry/src/main/java/control/Main.java
@@ -1,10 +1,24 @@
 package control;
 
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.nio.file.Paths;
+
 /**
  * Created by bas on 12-12-15.
  */
 public class Main {
     public static void main(String[] args){
+        playSong();
+    }
 
+    private void playSong(){
+        String bip = "bip.mp3";
+        JFXPanel fxPanel = new JFXPanel();
+        Media hit = new Media(Paths.get(bip).toUri().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(hit);
+        mediaPlayer.play();
     }
 }
