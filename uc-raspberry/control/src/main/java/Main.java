1aa645dd5801401cf860aae7fa7fd86d0362b740
Bas de Bruijn <s.f.debruijn@gmail.com>
2015-12-13 19:55:28 +0100

    re-add playing mp3

--- uc-raspberry/control/src/main/java/Main.java
+++ uc-raspberry/control/src/main/java/Main.java
@@ -1,8 +1,25 @@
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.nio.file.Paths;
+
 /**
  * Created by bas on 12-12-15.
  */
 public class Main {
-    public static void main(String[] args){
+    private static MediaPlayer mediaPlayer;
 
+    public static void main(String[] args){
+        playSong();
+    }
+
+    private static void playSong(){
+         String bip = "control/bip.mp3";
+         JFXPanel fxPanel = new JFXPanel();
+         Media hit = new Media(Paths.get(bip).toUri().toString());
+         mediaPlayer = new MediaPlayer(hit);
+         mediaPlayer.play();
     }
 }
+
