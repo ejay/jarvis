91c34ff96a2a960f9b758ff7ddd5783b6438dda4
Bas de Bruijn <s.f.debruijn@gmail.com>
2015-12-31 19:56:10 +0100

    add NSocket class

--- uc-raspberry/control/src/main/java/Main.java
+++ uc-raspberry/control/src/main/java/Main.java
@@ -1,7 +1,6 @@
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import storage.Storage;
 
 import java.nio.file.Paths;
 
@@ -14,13 +13,36 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 //        playSong();
 
-        Storage storage = new Storage("localhost");
+//        Storage storage = new Storage("localhost");
+//
+//        while(true){
+//            String value = storage.get("light");
+//            System.out.println("light value = " + value);
+//            Thread.sleep(3*1000);
+//        }
 
-        while(true){
-            String value = storage.get("light");
-            System.out.println("light value = " + value);
-            Thread.sleep(3*1000);
-        }
+        NSocket.connect("192.168.178.69", 5001);
+        System.out.println(NSocket.write("{\"action\":\"identify\"}"));
+        System.out.println(NSocket.write("{\"action\":\"request values\"}"));
+
+        String line = NSocket.getLine();
+
+        System.out.println("line = " + line);
+
+
+        System.out.println(NSocket.write("{\n" +
+                "\"action\": \"send\"\n" +
+                "\"code\": {\n" +
+                "\"protocol\": [\"kaku_switch\"],\n" +
+                "\"id\": 17432370,\n" +
+                "\"unit\": 0,\n" +
+                "\"off\": 1\n" +
+                "}\n" +
+                "}"));
+
+
+
+        NSocket.close();
 
     }
 
---
+++ uc-raspberry/control/src/main/java/NSocket.java
@@ -0,0 +1,134 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+public class NSocket {
+
+    private static Socket socket;
+    private static BufferedReader bufferedReader = null;
+    private static PrintStream printStream = null;
+    private static String line = null;
+
+    private static long heart = new Date().getTime();
+    private static long beat = new Date().getTime();
+    private static long timeout = 5000;
+    private static boolean isConnected = false;
+
+    public static class HeartBeat implements Runnable {
+        public void run() {
+            try {
+                while(!Thread.interrupted()) {
+                    NSocket.write("HEART");
+                    Thread.sleep(timeout/5);
+                    NSocket.heart = new Date().getTime();
+                }
+            } catch(InterruptedException e) {
+            }
+        }
+    }
+
+    public static Socket connect(String server, int port) {
+        try {
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(server, port), 1000);
+            socket.setSoTimeout(30000);
+        } catch (UnknownHostException e) {
+            //Log.printf("Don't know about host");
+            isConnected = false;
+            return null;
+        } catch(IOException e) {
+            //Log.printf("Could not get I/O for connection");
+            isConnected = false;
+            return null;
+        }
+        Thread thread = new Thread(new NSocket.HeartBeat());
+        thread.start();
+        isConnected = true;
+        return socket;
+    }
+
+    public static boolean connected() {
+        if(isConnected) {
+            if((NSocket.heart-NSocket.beat) > timeout) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static void close() {
+        try {
+            if(bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if(printStream != null) {
+                printStream.close();
+            }
+            if(socket != null) {
+                socket.close();
+            }
+        } catch(IOException e) {
+            //Log.printf("Could not close connection to server");
+        }
+    }
+
+    public static String getLine() {
+        return line;
+    }
+
+    public static boolean read(int size) {
+        if(size == 0) {
+            size = 1025;
+        }
+        try {
+            if(bufferedReader == null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"), size);
+            }
+            if(bufferedReader.ready()) {
+                if((line = bufferedReader.readLine()) != null) {
+                    if(new String("BEAT").equals(line)) {
+                        NSocket.beat = new Date().getTime();
+                        return false;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } catch(IOException e) {
+            isConnected = false;
+            //Log.printf("Failed to receive messages from server");
+        }
+        return true;
+    }
+
+    public static boolean write(String message) {
+        try {
+            if(printStream == null) {
+                printStream = new PrintStream(socket.getOutputStream(), false);
+            }
+            printStream.print(message+"\n");
+            printStream.flush();
+            Thread.sleep(10);
+            return true;
+        } catch(InterruptedException e) {
+            isConnected = false;
+            //Log.printf("Failed to receive messages from server");
+        } catch(IOException e) {
+            isConnected = false;
+            //Log.printf("Failed to write messages to server");
+        }
+        return false;
+    }
+}
