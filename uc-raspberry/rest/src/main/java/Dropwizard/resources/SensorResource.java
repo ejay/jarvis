8e2512ba29c4cd1e1fcdd55e4b45bf24d3ad46fd
Bas de Bruijn <s.f.debruijn@gmail.com>
2015-12-31 10:38:31 +0100

    fix POST method

--- uc-raspberry/rest/src/main/java/Dropwizard/resources/SensorResource.java
+++ uc-raspberry/rest/src/main/java/Dropwizard/resources/SensorResource.java
@@ -8,20 +8,20 @@ import javax.ws.rs.core.MediaType;
 import java.util.logging.Logger;
 
 @Path("/sensor-data")
-@Produces(MediaType.APPLICATION_JSON)
 public class SensorResource {
     private final static Logger logger = Logger.getLogger(SensorResource.class.getName());
 
     @POST
-    @Timed
-    public void receiveSensorData(@QueryParam("timestamp") Optional<String> timestamp,
-                           @QueryParam("sensorId") Optional<String> sensorId,
-                           @QueryParam("sensorType") Optional<String> sensorType,
-                           @QueryParam("value") Optional<String> value) {
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void receiveSensorData(@FormParam("timestamp") Optional<String> timestamp,
+                           @FormParam("sensorId") Optional<String> sensorId,
+                           @FormParam("sensorType") Optional<String> sensorType,
+                           @FormParam("value") Optional<String> value) {
         logger.info(String.format("Received sensor data: timestamp = %s, sensorId = %s, sensorType = %s, value = %s",
                 timestamp, sensorId, sensorType, value));
 //                timestamp.get(), sensorId.get(), sensorType.get(), value.get()));
         // http://localhost:8080/sensor-data?timestamp=time3&sensorId=sensorid3&sensorType=sensortype3&value=value3
         // http://localhost:8080/sensor-data/timestamp/time3/sensorId/sensorid3/sensorType/sensortype3/value/value3
+        // curl -i -d "timestamp=time3&sensorId=sensorid3&sensorType=sensortype3&value=value3" http://localhost:8080/sensor-data
     }
 }
