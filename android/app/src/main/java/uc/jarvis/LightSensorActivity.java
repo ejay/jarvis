f9dc223db8a37ba4def929be41540e1ca3ca86c5
ejay <jager.erik@gmail.com>
2016-01-24 00:07:31 +0100

    LightSensorActivity runs in the background when screen is off

--- android/app/src/main/java/uc/jarvis/LightSensorActivity.java
+++ android/app/src/main/java/uc/jarvis/LightSensorActivity.java
@@ -6,6 +6,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -14,6 +16,12 @@ import java.util.Calendar;
 public class LightSensorActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sensorManager;
     private Sensor light;
+    private double lastLightValue;
+
+    private static final int UPDATE_INTERVAL_LIGHT = 5*100; //5ms interval;
+    private static final int LIGHT_THRESHOLD = 100;
+    private HandlerThread mSensorThread;
+    private Handler mSensorHandler;
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,11 @@ public class LightSensorActivity extends AppCompatActivity implements SensorEven
         // a particular sensor.
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        mSensorThread = new HandlerThread("Sensor processing thread", Thread.MAX_PRIORITY);
+        mSensorThread.start();
+        mSensorHandler = new Handler(mSensorThread.getLooper());
+        sensorManager.registerListener(mListener, light, UPDATE_INTERVAL_LIGHT, UPDATE_INTERVAL_LIGHT, mSensorHandler);
     }
 
     @Override
@@ -35,19 +48,52 @@ public class LightSensorActivity extends AppCompatActivity implements SensorEven
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.i("UPDATE", "Light sensor value: " + event.values[0]);
+//        Log.i("UPDATE", "Light sensor value: " + event.values[0]);
+//
+//        String postString = String.format("timestamp=%s&sensorId=%s&sensorType=%s&value=%s",
+//                Long.toString(Calendar.getInstance().getTime().getTime()),
+//                "001",
+//                "Light",
+//                Float.toString(event.values[0]));
 
-        String postString = String.format("timestamp=%s&sensorId=%s&sensorType=%s&value=%s",
-                Long.toString(Calendar.getInstance().getTime().getTime()),
-                "001",
-                "Light",
-                Float.toString(event.values[0]));
-
-        new PostSensorDataTask().execute(postString);
+//        new PostSensorDataTask().execute(postString);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+
+    private final SensorEventListener mListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            int sensorType = event.sensor.getType();
+
+            switch (sensorType){
+                case Sensor.TYPE_LIGHT:
+                    processLightEvent(event);
+            }
+        }
+
+        protected void processLightEvent(SensorEvent event){
+            if (Math.abs(lastLightValue - event.values[0]) >= LIGHT_THRESHOLD) {
+                Log.i("UPDATE", "Light sensor value: " + event.values[0]);
+
+                String postString = String.format("key=%s&value=%s",
+                        "RoomLight_raw",
+                        Float.toString(event.values[0]));
+
+                lastLightValue = event.values[0];
+
+                new PostSensorDataTask().execute(postString);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
